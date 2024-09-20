7aaf16bd16751ef2786b191674be136529ec391a
AhmedRayyan44 <165449090+AhmedRayyan44@users.noreply.github.com>
2024-09-20 04:10:00 +0300

    Update dzrt_notificationLocalimages.py

--- dzrt_notificationLocalimages.py
+++ dzrt_notificationLocalimages.py
@@ -9,7 +9,7 @@ sent_products = []
 # Dictionary to store the time each product was last sent
 product_send_times = {}
 # List of products that have special handling
-special_products = ["بيربل مست", "هايلاند بيريز", "سبايسي زيست", "هيلة", "تمرة", "جاردن منت", "إيدجي منت", "سبايسي زيست", "ايسي رش", "سي سايد فروست", "منت فوجن", ""]
+special_products = ["بيربل مست", "هايلاند بيريز", "سبايسي زيست", "هيلة", "تمرة", "جاردن منت", "إيدجي منت", "سمرة", "ايسي رش", "سي سايد فروست", "منت فيوجن", ""]
 # List of products to exclude from sending
 excluded_products = []
 # Variable to store the time of the last clearing of the sent_products list
